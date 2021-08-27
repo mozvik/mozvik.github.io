59a853d8638adc34f8de82f63ea77b24ea7968a7
mozvik <mozvik@yahoo.com>
2021-08-27 21:47:50 +0200

    v1

--- src/components/ButtonDownload.vue
+++ src/components/ButtonDownload.vue
@@ -1,15 +1,26 @@
 <template>
-  <button>
-     <slot></slot>
+  <button @mouseover="moveIcon"
+          @mouseleave="hover = false"
+          :class="{ active: hover }">
+          <span class="dlbtn-icon"><Icon icon="uiw:login" width="48" /></span>
+          <span class="dlbtn-text">
+            <slot></slot>
+          </span>
+          
   </button>
+  <span></span>
 </template>
 
 <script>
+import { Icon } from '@iconify/vue';
 export default {
   name: "ButtonDownload",
+  components:{
+    Icon,
+  },
   data () {
     return {
-      
+      hover: false,
     }
   },
   props: {
@@ -17,7 +28,23 @@ export default {
     displaySize: Number,
     activeMenuItem: Number,
   },
-  emits: ["selectedMenuItem"]
+  emits: ["selectedMenuItem"],
+  methods: {
+   moveIcon(e){
+     const span = e.target.querySelector('i')
+     if (span) {
+       span.classList.remove('reseticon')
+     span.classList.add('downwards')
+     }
+     
+     console.log('on :>> ', span);
+   },
+   resetIcon(e){
+     e.target.querySelector('i').classList.remove('downwards')
+     e.target.querySelector('i').classList.add('reseticon')
+    console.log('out :>> ', e.target.querySelector('i'));
+   }
+  }
   
 };
 </script>
@@ -25,44 +52,57 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 button {
-  border-color: #8e44ad;
-  border-color: red;
-  border-radius: 5px;
-  color: #8e44ad;
-  position: relative;
+  text-transform: uppercase;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 500;
+  font-size: 24px;
+  border-radius: 3px;
+  border: 3px solid var(--light);
+  color: var(--light);
   overflow: hidden;
-  z-index: 1;
-  -webkit-transition: color 150ms ease-in-out;
-  transition: color 150ms ease-in-out;
-  margin: 20px;
-  padding: 1.2em 2.8em;
+  transition: color 300ms ease-in-out;
+  margin: 0 auto;
+  padding: 1.0em 4.8em;
+   display: flex; 
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  transition: all 300ms ease-in-out;
 } 
-button::after{
-  content: '';
-  position: absolute;
-  display: block;
-  border-radius: 50%;
-  top: -150%;
-  left: 0;
-  -webkit-transform: translateX(-50%);
-          transform: translateX(-50%);
-  width: 0%;
-  height: 400%;
-  background: var(--green);
-  z-index: -1;
-  -webkit-transition: all 300ms ease-in-out;
-  transition: all 300ms ease-in-out;
-}
-button:hover{
-color: #fff;
-}
-button:hover:after {
-  width: 230%;
-  left: 50%;
-  top: -150%;
-  height: 400%;
-   -webkit-transition: all 300ms ease-in-out;
-  transition: all 300ms ease-in-out;
-}
 
+button:hover{
+color: var(--darkest);
+background: var(--light);
+border: 3px solid var(--light);
+cursor: pointer;
+transition: all 300ms ease-in-out;
+}
+button > .dlbtn-icon{
+  color: var(--green);
+  transform: translate(-.5rem, 7rem);
+  display: inline-block;
+  text-align: left;
+  font-size: 24px;
+  transition: all 300ms ease-in-out;
+}
+button:hover > .dlbtn-icon{
+  margin: 0;
+  padding: 0;
+  color: var(--green);
+  transform: translate(-.5rem, 0);
+  transition: all 300ms ease-in-out;
+}
+button > .dlbtn-text{
+  color: var(--light);
+  transform: translate(-1rem, 0rem);
+  display: inline-block;
+  text-align: center;
+  font-size: 24px;
+  transition: all 300ms ease-in-out;
+}
+button:hover > .dlbtn-text{
+  color: var(--darkest);
+  transform: translate(1.5rem, 0);
+  transition: all 300ms ease-in-out;
+}
 </style>
