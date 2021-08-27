b424ab0adb67170d6cf19109cb270f3e3d2a5788
mozvik <mozvik@yahoo.com>
2021-08-27 17:41:42 +0200

    content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,24 @@
 <template>
    <div class="section" id="about">
-    <h1>About</h1>
+    <div>
+      <h2 class="title">About</h2>
+    </div>
+    <p>I have a serious passion for web development, it provides me the opportunity of both creativity and programming.</p>
+    <p> I love experimenting with UI animations and effects, build something, or just solving problems.</p>
+    <div>
+      <ButtonDownload>Download CV in PDF format</ButtonDownload>
+    </div>
   </div>
 </template>
 
 <script>
+import ButtonDownload from "@/components/ButtonDownload.vue";
 export default {
+  components: {ButtonDownload,},
   name: "About",
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      
     }
   }
   // props: {
--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="section" id="skills">
     <h1>skills</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi itaque quam excepturi, perspiciatis a facere reprehenderit fugit similique aliquam maiores at id nisi iusto pariatur dolores omnis quia, voluptate deleniti.
+    Reprehenderit expedita nulla culpa deserunt cumque pariatur deleniti ut, voluptate nisi vero architecto eveniet ad distinctio quia mollitia doloremque totam aperiam, delectus corporis voluptatibus sequi et voluptates enim corrupti! Perspiciatis.
+    Et quaerat quod magnam provident. Officiis fugit distinctio eius itaque, doloribus saepe asperiores nulla ad temporibus aut velit explicabo facere quibusdam beatae officia, ipsa perferendis, repellat dolorum minus obcaecati magnam.</p>
   </div>
 </template>
 
