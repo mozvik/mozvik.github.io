eee0b447d8266753f093749347fcc61a4d97ebe6
mozvik <mozvik@yahoo.com>
2021-09-04 10:55:44 +0200

    slider zero

---
+++ src/components/Slider.vue
@@ -0,0 +1,37 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: "Slider",
+  data() {
+    return {
+      menuActive: false,
+    };
+  },
+  props: {
+    navItems: Object,
+    displaySize: Number,
+    activeMenuItem: String,
+  },
+  emits: ["selectedMenuItem"],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+/****************************************************** */
+/**          TABLET                                   **/
+@media screen and (min-width: 768px) {
+  
+}
+/****************************************************** */
+/**          DESKTOP                                   **/
+@media screen and (min-width: 992px) {
+ 
+
+}
+</style>
