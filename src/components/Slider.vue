4a30e9c945b2dd9cdeb63e8c802e25adb71ef0b2
mozvik <mozvik@yahoo.com>
2021-09-14 21:15:39 +0200

    vueperslide

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,193 +1,368 @@
 <template>
-           <!-- <div  class="slider-container">
-
-            <div ref="sliderRef" class="slider-items"
-            :style="containerWidth"
-            >
-              <div class="slider-item"
-              :style="sliderStyle">
-                <div class="item-title">Quiz Application</div>
-                <div class="item-body">
-                  <div class="project-technology">
-                    <div>Vue.js</div>
-                    <div>Bootstrap</div>
-                    <div></div>
-                    <div></div>
-                  </div>
-                  <img src="https://picsum.photos/360/300/?rnd=1" alt="" />
-                </div>
-              </div>
-              <div class="slider-item"
-              :style="sliderStyle">
-                <div class="item-title">Puzzle Slider Application</div>
-                <div class="item-body">
-                  <div class="project-technology">
-                    <div>Vue.js</div>
-                    <div>Bootstrap</div>
-                    <div></div>
-                    <div></div>
-                  </div>
-                  <img src="https://picsum.photos/360/300/?rnd=2" alt="" />
-                </div>
-              </div>
-              <div class="slider-item"
-              :style="sliderStyle">
-                <div class="item-title">Movie Database Application</div>
-                <div class="item-body">
-                  <div class="project-technology">
-                    <div><i class="devicon-angularjs-plain-wordmark colored"></i></div>
-                    <div>Bootstrap</div>
-                    <div></div>
-                    <div></div>
-                   </div>
-                  <img src="https://picsum.photos/360/300/?rnd=3" alt="" />
-                </div>
-              </div>
-              <div class="slider-item"
-              :style="sliderStyle">
-                <div class="item-title">EsoGuides - Gaming Website/Magazine</div>
-                <div class="item-body">
-                  <div class="project-technology">
-                    <div>Vue.js</div>
-                    <div>Bootstrap</div>
-                    <div>Php</div>
-                    <div>Strapi</div>
-                  </div>
-                  <img src="https://picsum.photos/360/300/?rnd=4" alt="" />
-                </div>
-              </div>
-              <div class="slider-item"
-              :style="sliderStyle">
-                <div class="item-title">coming soon...</div>
-                <div class="item-body">
-                  <div class="project-technology">
-                    <div>Vue.js</div>
-                    <div>Bootstrap</div>
-                    <div></div>
-                    <div></div>
-                  </div>
-                  <img src="https://picsum.photos/360/300/?rnd=5" alt="" />
-                </div>
+  <vueper-slides
+    class="no-shadow"
+    :dragging-distance="50"
+    :breakpoints="breakpoints"
+    fade
+    bullets-outside
+    prevent-y-scroll
+  >
+    <vueper-slide
+      v-for="(card, i) in cards"
+      :key="i"
+      :title="card.title"
+      :content="card.content"
+      class="card-item"
+      :style="'background-color: var(--background)'"
+    >
+      <template #content>
+        <div class="item-body">
+          <div class="item-title">
+            <div class="item-name">{{ card.title }}</div>
+            <div class="item-tech">
+              <div
+                class="item-tech-icons"
+                v-for="(tech, j) in card.technology"
+                :key="j"
+              >
+                <Icon :icon="tech.icon" :width="tech.width" />
               </div>
             </div>
-            <div class="slider-dots"></div>
-            <div class="slider-prev-next"></div>
-          </div>  -->
-   
-  <!-- fixed-height="450px" -->
-<vueper-slides class="no-shadow" :dragging-distance="50" :breakpoints="breakpoints">
-  
-  <vueper-slide
-    v-for="(slide, i) in slides"
-    :key="i"
-    :title="slide.title"
-    :content="slide.content"
-    :style="'background-color: ' + ['#ff5252', '#42b983'][i % 2]">
-    <template #content>
-      <div class='slider-item'>
-        <div class='item-title'>{{slide.title}}</div>
-        <div class="item-image"><img :src="slide.image" alt=""></div>
-        <div class='item-body'>
-          <div class="item-brief">{{slide.brief}}</div>
-          <div class="item-description">{{slide.description}}</div>
-          <div class="item-technology">{{slide.technology}}</div>
-          <div class="item-buttons">
-            <div class="item-demo"></div>
-            <div class="item-code"></div>
+          </div>
+          <div class="item-brief">{{ card.brief }}</div>
+          <div class="item-description">{{ card.description }}</div>
+        </div>
+        <div class="item-image">
+          <img :src="displaySize < 2 ? '' : card.imageDesktop" alt="" />
+        </div>
+
+        <div class="item-buttons">
+          <div class="item-demo">
+            <Button :options="card.buttonDemo"></Button>
+          </div>
+          <div class="item-code">
+            <span></span><Button :options="card.buttonCode"></Button>
           </div>
         </div>
-      </div>
-    </template>
-    
+      </template>
     </vueper-slide>
-</vueper-slides>
- 
+  </vueper-slides>
 </template>
 
 <script>
-
-import { VueperSlides, VueperSlide } from 'vueperslides'
-import 'vueperslides/dist/vueperslides.css'
-
+import { VueperSlides, VueperSlide } from "vueperslides";
+import "vueperslides/dist/vueperslides.css";
+import { Icon } from "@iconify/vue";
+import Button from "@/components/Button.vue";
 export default {
   name: "Slider",
-  components: { VueperSlides, VueperSlide },
-  
-    data() {
+  components: { VueperSlides, VueperSlide, Icon, Button },
+
+  data() {
     return {
-       breakpoints: {
-    1980: {
-    // slideRatio: 1 / 3,
-    fixedHeight: '500px'
-    },
-    1200: {
-      slideRatio: 1 / 2,
-      fixedHeight: '400px',
-    },
-    992: {
-      slideRatio: 2 / 3
-    },
-    600: {
-      slideRatio: 1 / 1,
-      arrows: false,
+      breakpoints: {
+        1980: {
+          fixedHeight: "400px",
+        },
+        1200: {
+          fixedHeight: "400px",
+        },
+        992: {
+          fixedHeight: "400px",
+          bulletsOutside: true,
+        },
+        600: {
+          arrows: true,
+          bulletsOutside: true,
+          fixedHeight: "480px",
+        },
+      },
 
+      cards: [
+        {
+          title: "Quiz Application",
+          imageMobile: "quiz-m.jpg",
+          imageDesktop: "https://via.placeholder.com/300x400.jpg",
+          tech: "Vue, Vuex, Ajax",
+          brief:
+            "Simple Quiz Application made with Vue.js & Vuex state management",
+          description:
+            "This application gets JSON data from the Open Trivia Database API (https://opentdb.com/api_config.php) using Ajax requests and saves the user's high scores to the local storage.",
+          technology: [
+            {
+              icon: "logos:html-5",
+              width: 24,
+            },
+            {
+              icon: "logos:css-3",
+              width: 24,
+            },
+            {
+              icon: "logos:vue",
+              width: 30,
+            },
+          ],
+          buttonDemo: {
+            text: "View Demo",
+            icon: "healthicons:eye",
+            anchor: "https://mozvik.github.io/vue-quiz/",
+          },
+          buttonCode: {
+            text: "Github Repository",
+            icon: "akar-icons:github-outline-fill",
+            anchor: "https://github.com/mozvik/vue-quiz",
+          },
+        },
+        {
+          title: "Puzzle Slider",
+          imageMobile: "https://picsum.photos/360/300/?rnd=2",
+          imageDesktop: "https://via.placeholder.com/300x400.jpg",
+          tech: "Vue, Vuex, Ajax",
+          brief:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt id quis porro?",
+          description:
+            "Dolorum qui minima consectetur accusantium quisquam cum numquam dolor ex, earum, fugiat temporibus natus dignissimos aut reprehenderit nihil! ",
+          technology: [
+            {
+              icon: "logos:html-5",
+              width: 24,
+            },
+            {
+              icon: "logos:css-3",
+              width: 24,
+            },
+            {
+              icon: "logos:vue",
+              width: 30,
+            },
+          ],
+          buttonDemo: {
+            text: "View Demo",
+            icon: "healthicons:eye",
+            anchor: "https://mozvik.github.io/vue-quiz/",
+          },
+          buttonCode: {
+            text: "Github Repository",
+            icon: "akar-icons:github-outline-fill",
+            anchor: "https://github.com/mozvik/vue-quiz",
+          },
+        },
+        {
+          title: "Movie Database App",
+          imageMobile: "https://picsum.photos/360/300/?rnd=3",
+          imageDesktop: "https://via.placeholder.com/300x400.jpg",
+          tech: "React",
+          brief:
+            "Fuga voluptas consectetur nemo corporis temporibus velit atque ipsa, rem excepturi quas mollitia modi quos.",
+          description:
+            "Cumque debitis, natus consequuntur facilis, reprehenderit reiciendis quas facere voluptates ut adipisci repudiandae quasi error rerum sunt, eum expedita qui nam aliquam est perspiciatis ullam voluptatem quibusdam voluptas hic.",
+          technology: [
+            {
+              icon: "logos:html-5",
+              width: 24,
+            },
+            {
+              icon: "logos:css-3",
+              width: 24,
+            },
+            {
+              icon: "logos:react",
+              width: 28,
+            },
+          ],
+          buttonDemo: {
+            text: "View Demo",
+            icon: "healthicons:eye",
+            anchor: "https://mozvik.github.io/vue-quiz/",
+          },
+          buttonCode: {
+            text: "Github Repository",
+            icon: "akar-icons:github-outline-fill",
+            anchor: "https://github.com/mozvik/vue-quiz",
+          },
+        },
+        {
+          title: "Recipe Book App",
+          imageMobile: "https://picsum.photos/360/300/?rnd=4",
+          imageDesktop: "https://via.placeholder.com/300x400.jpg",
+          tech: "Angular",
+          brief:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, deleniti laboriosam!",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor possimus a totam earum. Beatae veritatis quia explicabo deserunt. Minima hic quasi eius.",
+          technology: [
+            {
+              icon: "logos:angular-icon",
+              width: 26,
+            },
+            {
+              icon: "logos:php",
+              width: 50,
+            },
+          ],
+          buttonDemo: {
+            text: "View Demo",
+            icon: "healthicons:eye",
+            anchor: "https://mozvik.github.io/vue-quiz/",
+          },
+          buttonCode: {
+            text: "Github Repository",
+            icon: "akar-icons:github-outline-fill",
+            anchor: "https://github.com/mozvik/vue-quiz",
+          },
+        },
+        {
+          title: "EsoGuides - Gaming Website",
+          imageMobile: "https://picsum.photos/360/300/?rnd=5",
+          imageDesktop: "https://via.placeholder.com/300x400.jpg",
+          tech: "React",
+          brief:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, deleniti laboriosam!",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor possimus a totam earum. Beatae veritatis quia explicabo deserunt. Minima hic quasi eius.",
+          technology: [
+            {
+              icon: "logos:react",
+              width: 26,
+            },
+            {
+              icon: "logos:php",
+              width: 50,
+            },
+          ],
+          buttonDemo: {
+            text: "View Demo",
+            icon: "healthicons:eye",
+            anchor: "https://mozvik.github.io/vue-quiz/",
+          },
+          buttonCode: {
+            text: "Github Repository",
+            icon: "akar-icons:github-outline-fill",
+            anchor: "https://github.com/mozvik/vue-quiz",
+          },
+        },
+      ],
+    };
+  },
+  props: {
+    displaySize: {
+      type: Number,
     },
- 
+  },
 
-   
-  },
-         slides: [
-          {
-            title: 'Quiz Application',
-            image: 'https://picsum.photos/360/300/?rnd=1',
-            tech: 'Vue, Vuex, Ajax',
-            brief: 'Simple Quiz Application made with Vue.js & Vuex state management',
-            description: 'This application gets JSON data from the Open Trivia Database API (https://opentdb.com/api_config.php) using Ajax requests and saves the user\'s high scores to the local storage.',
-            content: "",
-          },{
-            title: 'Slide #2',
-            tech: '',
-            content: "<div class='slider-item' >            <div class='item-title'>Quiz Application</div>                <div class='item-body'>                  <div class='project-technology'>                    <div>Vue.js</div>                    <div>33333</div>                    <div></div>                    <div></div>                  </div>                  <img src='https://picsum.photos/360/300/?rnd=1' alt='' />                </div>              </div>",
-          }
-  ] 
-    }
-    
-  },
-  
-   
-  mounted(){
-   
-  },
-  computed: {
-    
-  },
+  mounted() {},
+  computed: {},
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.item {
-  background: black;
+.item-image {
+  display: none;
 }
-.vueperslides{
-  /* width: 100%;
-  height: 70vh; */
+
+.card-item {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.card-item:hover .item-title {
+  color: var(--light);
+  transition: color 0.4s ease-out;
+}
+.item-buttons button {
+  width: 100%;
+}
+.item-title {
+  background: var(--background);
+  font-family: "Oswald", Helvetica, sans-serif;
+  color: var(--light100);
+  font-size: 1.4rem;
+  padding: 0.5rem 0;
+  text-transform: uppercase;
+  transition: color 0.4s ease-out;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.item-body {
+  font-family: "Roboto", Helvetica, sans-serif;
+  display: flex;
+  flex-direction: column;
+  font-size: 1.1rem;
+  padding: 0 0.5rem;
+  /* background: var(--background800); */
+}
+.item-brief {
+  opacity: .9;
+  /* background: var(--background800); */
+  color: var(--light200);
+  padding: 1rem 0;
+}
+.item-description {
+  color: var(--light300);
+  opacity: .8;
+  /* background: var(--background800); */
+  padding: 1rem 0;
+}
+.item-tech-icons {
+  display: inline-block;
+  padding: 0 0.2rem;
+}
+.item-buttons {
+  /* border: 1px solid var(--light); */
 }
 
 /****************************************************** */
 /**          TABLET                                   **/
 @media screen and (min-width: 768px) {
-  
+  .item-image img {
+    display: none;
+  }
 }
 /****************************************************** */
 /**          DESKTOP                                   **/
 @media screen and (min-width: 992px) {
- 
- /* .ex--center-mode {
-  width: 1600px;
-  max-width: 1600px;
-  margin: auto;
-}  */
+  .item-buttons {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+  }
+  
+}
+/**          LARGE DESKTOP                                   **/
+@media screen and (min-width: 1200px) {
+  .item-image {
+    display: block;
+  }
+  .item-image img {
+    height: auto;
+    max-width: 300px;
+    display: block;
+    margin-right: auto;
+  }
 
+  .card-item {
+    display: grid;
+    grid-auto-columns: 1fr auto;
+    grid-auto-rows: 1fr auto;
+  }
+  .item-image {
+    grid-row: 1/3;
+    grid-column: 1;
+  }
+  .item-body {
+    grid-row: 1;
+    grid-column: 2;
+  }
+  .item-buttons {
+    grid-row: 2;
+    grid-column: 2;
+  }
+  .item-title{
+    font-size: 1.5rem;
+  }
 }
 </style>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -9,9 +9,12 @@
         
       </div>
       <div class="projects-container">
-          <Slider></Slider>
+        <Slider :displaySize="displaySize"></Slider>
       </div>
     </div>
+    <div class="parallax-layer parallax-layer-back back-title">
+      
+    </div> 
     <div class="parallax-layer parallax-layer-deep back-title">
       <h1>PORTFOLIO</h1>
     </div>  
@@ -24,66 +27,253 @@
 
 <script>
 import Slider from "@/components/Slider.vue";
+
 export default {
   name: "Portfolio",
    components:{
-     Slider,
+    Slider,
+    
    },
   data() {
     return {
-      msg: "Welcome to Your Vue.js App",
-    };
+      cards: [
+      {
+        title: 'Quiz Application',
+        imageMobile: 'quiz-m.jpg',
+        imageDesktop: 'quiz.jpg',
+        tech: 'Vue, Vuex, Ajax',
+        brief: 'Simple Quiz Application made with Vue.js & Vuex state management',
+        description: 'This application gets JSON data from the Open Trivia Database API (https://opentdb.com/api_config.php) using Ajax requests and saves the user\'s high scores to the local storage.',
+        technology: [
+          {
+            icon: 'logos:html-5',
+            width: 24
+          },
+          {
+            icon: 'logos:css-3',
+            width: 24
+          },
+          {
+            icon: 'logos:vue',
+            width: 30
+          },
+        ],
+        buttonDemo: 
+          {
+            text: 'View Demo',
+            icon: 'healthicons:eye',
+            anchor: 'https://mozvik.github.io/vue-quiz/',
+          },
+        buttonCode: 
+          {
+            text: 'Github Repository',
+            icon: 'akar-icons:github-outline-fill',
+            anchor: 'https://github.com/mozvik/vue-quiz',
+          },
+        
+      },{
+        title: 'Puzzle Slider',
+        imageMobile: 'https://picsum.photos/360/300/?rnd=2',
+        imageDesktop: 'https://picsum.photos/360/300/?rnd=2',
+        tech: 'Vue, Vuex, Ajax',
+        brief: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt id quis porro?',
+        description: 'Dolorum qui minima consectetur accusantium quisquam cum numquam dolor ex, earum, fugiat temporibus natus dignissimos aut reprehenderit nihil! ',
+        technology: [
+          {
+            icon: 'logos:html-5',
+            width: 24
+          },
+          {
+            icon: 'logos:css-3',
+            width: 24
+          },
+          {
+            icon: 'logos:vue',
+            width: 30
+          },
+        ],
+        buttonDemo: 
+          {
+            text: 'View Demo',
+            icon: 'healthicons:eye',
+            anchor: 'https://mozvik.github.io/vue-quiz/',
+          },
+        buttonCode: 
+          {
+            text: 'Github Repository',
+            icon: 'akar-icons:github-outline-fill',
+            anchor: 'https://github.com/mozvik/vue-quiz',
+          },
+        
+      },{
+        title: 'Movie Database App',
+        imageMobile: 'https://picsum.photos/360/300/?rnd=3',
+        imageDesktop: 'https://picsum.photos/360/300/?rnd=3',
+        tech: 'React',
+        brief: 'Fuga voluptas consectetur nemo corporis temporibus velit atque ipsa, rem excepturi quas mollitia modi quos.',
+        description: 'Cumque debitis, natus consequuntur facilis, reprehenderit reiciendis quas facere voluptates ut adipisci repudiandae quasi error rerum sunt, eum expedita qui nam aliquam est perspiciatis ullam voluptatem quibusdam voluptas hic.',
+        technology: [
+          {
+            icon: 'logos:html-5',
+            width: 24
+          },
+          {
+            icon: 'logos:css-3',
+            width: 24
+          },
+          {
+            icon: 'logos:react',
+            width: 28
+          },
+        ],
+        buttonDemo: 
+          {
+            text: 'View Demo',
+            icon: 'healthicons:eye',
+            anchor: 'https://mozvik.github.io/vue-quiz/',
+          },
+        buttonCode: 
+          {
+            text: 'Github Repository',
+            icon: 'akar-icons:github-outline-fill',
+            anchor: 'https://github.com/mozvik/vue-quiz',
+          },
+        
+      },{
+        title: 'Recipe Book App',
+        imageMobile: 'https://picsum.photos/360/300/?rnd=4',
+        imageDesktop: 'https://picsum.photos/360/300/?rnd=4',
+        tech: 'Angular',
+        brief: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, deleniti laboriosam!',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor possimus a totam earum. Beatae veritatis quia explicabo deserunt. Minima hic quasi eius.',
+        technology: [
+          
+          {
+            icon: 'logos:angular-icon',
+            width: 26
+          },
+          {
+            icon: 'logos:php',
+            width: 50
+          }
+        ],
+        buttonDemo: 
+          {
+            text: 'View Demo',
+            icon: 'healthicons:eye',
+            anchor: 'https://mozvik.github.io/vue-quiz/',
+          },
+        buttonCode: 
+          {
+            text: 'Github Repository',
+            icon: 'akar-icons:github-outline-fill',
+            anchor: 'https://github.com/mozvik/vue-quiz',
+          },
+        
+      },{
+        title: 'EsoGuides - Gaming Website',
+        imageMobile: 'https://picsum.photos/360/300/?rnd=5',
+        imageDesktop: 'https://picsum.photos/360/300/?rnd=5',
+        tech: 'React',
+        brief: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, deleniti laboriosam!',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor possimus a totam earum. Beatae veritatis quia explicabo deserunt. Minima hic quasi eius.',
+        technology: [
+          
+          {
+            icon: 'logos:react',
+            width: 26
+          },
+          {
+            icon: 'logos:php',
+            width: 50
+          }
+        ],
+        buttonDemo: 
+          {
+            text: 'View Demo',
+            icon: 'healthicons:eye',
+            anchor: 'https://mozvik.github.io/vue-quiz/',
+          },
+        buttonCode: 
+          {
+            text: 'Github Repository',
+            icon: 'akar-icons:github-outline-fill',
+            anchor: 'https://github.com/mozvik/vue-quiz',
+          },
+        
+      },
+    ] 
+  }
+    
   },
-  // props: {
-  //   msg: String,
-  // },
+  props: {
+    
+    displaySize: Number,
+    
+  },
+  mounted() {
+   
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+#portfolio{
+  height: 100vh;
+  /* pointer-events: none;   */
+} 
+
 #portfolio .content{
-    
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     justify-content: start;
-     padding-left: 1rem;
+    padding-left: 1rem;
     padding-right: 1rem;
-    height: 100vh;
-
-  }
+    /* height: 100vh; */
+    }
   .projects-container{
     width: 100%;
     height: auto;
-    padding-top: 5rem;
-  }
+    padding-top: 2.5rem;
+    
+    }
+    
   /**          LAPTOP TABLET                                   **/
 @media screen and (min-width: 992px) {
 #portfolio .content{
-  width: calc(100vw - 250px);
-
-  }
- 
-
-}
-/**          LARGE DESKTOP                                   **/
-@media screen and (min-width: 1200px) {
-  #portfolio .content{
+  width: calc(100vw - 250px - 1rem);
+  }#portfolio .content{
     
    display: flex;
    flex-direction: column;
    align-items: start;
-   justify-content: space-around;
+   justify-content: space-between;
    height: 100vh;
-   padding: 0 4rem;
-
+   padding: 0 1rem;
+   /* padding-bottom: 1rem; */
   }
-  
   #portfolio .content .projects-container{
     
-    max-width: 1800px;
-    padding-bottom: 2rem;
+
+  }
+}
+/**          LARGE DESKTOP                                   **/
+@media screen and (min-width: 1200px) {
+  #portfolio{
+  height: 100vh;
+}
+   #portfolio .content{
+     padding-left: 5.5rem;
+     padding-right: 5.5rem;
+   }
+  
+  #portfolio .content .projects-container, .card-item{
+    width: 100%;
+    max-width: 1600px;
+    /* margin: 0 auto; */
   }
 }
 </style>
